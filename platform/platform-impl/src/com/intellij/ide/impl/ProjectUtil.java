ed7695d11fab56409c76c002ec7e8d515d8a2ec5
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2022-09-06 22:28:16 +0200

    Romolo fix: if a open project processor is available for the specified file, and it returned null in openOrImport, skip temporaney project creation

--- platform/platform-impl/src/com/intellij/ide/impl/ProjectUtil.java
+++ platform/platform-impl/src/com/intellij/ide/impl/ProjectUtil.java
@@ -703,6 +703,17 @@ public final class ProjectUtil extends ProjectUtilCore {
           LOG.debug(location + ": canceled project opening");
           return null;
         }
+
+        // ROMOLO fix: if a open project processor is available for the specified file, and it returned null in openOrImport, skip temporaney project creation
+        NullableLazyValue<VirtualFile> lazyVirtualFile = NullableLazyValue.lazyNullable(() -> getFileAndRefresh(file));
+        for (ProjectOpenProcessor provider : ProjectOpenProcessor.EXTENSION_POINT_NAME.getIterable()) {
+          if (!(provider instanceof PlatformProjectOpenProcessor)) {
+            VirtualFile virtualFile = lazyVirtualFile.getValue();
+            if (virtualFile != null && provider.canOpenProject(virtualFile)) {
+              return null;
+            }
+          }
+        }
       }
     }
     catch (ProcessCanceledException ex) {
